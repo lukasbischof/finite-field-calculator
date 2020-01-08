fd9fa8c281660a9aa11472bf197be89eba935255
Lukas Bischof <me@luk4s.dev>
2020-01-08 14:09:58 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+require 'terminal-table'
+
+class Calculator
+  Z = 11
+
+  def initialize(z)
+
+  end
+end
+
+# rows = [
+#   %w[one two],
+#   %w[three four],
+#   %w[five six],
+# ]
+
+# puts Terminal::Table.new(rows: rows)
